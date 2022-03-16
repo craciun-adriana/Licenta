973451106a0d251233dfa6be46375af93c3b7e99
craciun-adriana <81712027+craciun-adriana@users.noreply.github.com>
2022-03-16 22:46:17 +0200

    Implemented book repository

---
+++ LicentaAPI/Persistence/Repositories/EFBookRepository.cs
@@ -0,0 +1,81 @@
+﻿using LicentaAPI.Persistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LicentaAPI.Persistence.Repositories
+{
+    public class EFBookRepository : IBookRepo
+    {
+        private AppDbContext _dbContext;
+
+        public EFBookRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        ///<inheritdoc />
+        public void Add(Book entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            _dbContext.Books.Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        ///<inheritdoc />
+        public void Delete(Book entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            _dbContext.Books.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        ///<inheritdoc />
+        public IEnumerable<Book> FindBookByTitle(string title)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException(nameof(title));
+            }
+
+            return _dbContext.Books.Where(book => book.Title.Contains(title)).ToList();
+        }
+
+        ///<inheritdoc />
+        public Book GetById(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            return _dbContext.Books.FirstOrDefault(book => book.ID.Equals(id));
+        }
+
+        ///<inheritdoc />
+        public IEnumerable<Book> Query(PaginationQuery paginationQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        ///<inheritdoc />
+        public void Update(Book entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            _dbContext.Update(entity);
+        }
+    }
+}
---
+++ LicentaAPI/Persistence/Repositories/IBookRepo.cs
@@ -0,0 +1,18 @@
+﻿using LicentaAPI.Persistence.Models;
+using System.Collections.Generic;
+
+namespace LicentaAPI.Persistence.Repositories
+{
+    /// <summary>
+    /// Interface providing contracts for <see cref="Book"/> repository.
+    /// </summary>
+    public interface IBookRepo : IGenericRepo<Book>
+    {
+        /// <summary>
+        /// Retrieves books that have the given title.
+        /// </summary>
+        /// <param name="title">The title of the book that the user is searching for.</param>
+        /// <returns></returns>
+        public IEnumerable<Book> FindBookByTitle(string title);
+    }
+}
