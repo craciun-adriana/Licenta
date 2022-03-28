7388293c15a9fe53521231d18a193cad77bcdd3e
craciun-adriana <81712027+craciun-adriana@users.noreply.github.com>
2022-03-28 21:06:52 +0300

    Added register operation.

---
+++ LicentaAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,52 @@
+﻿using LicentaAPI.Controllers.Models;
+using LicentaAPI.Infrastructure.Mapper;
+using LicentaAPI.Persistence.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LicentaAPI.Controllers
+{
+    [ApiController]
+    [Route("licenta/auth")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IMappingCoordinator _mapper;
+
+        public AuthenticationController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IMappingCoordinator mapper)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUserAsync(RegisterRequest request)
+        {
+            var user = new AppUser
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                DateOfBirth = request.DateOfBirth.Value,
+                Sex = request.Sex
+            };
+
+            // The password is taken separately to encrypt.
+            var registerResult = await _userManager.CreateAsync(user, request.Password);
+
+            if (registerResult.Succeeded)
+            {
+                // TODO: return created.
+                return Ok();
+            }
+
+            return BadRequest(registerResult.Errors);
+        }
+    }
+}
---
+++ LicentaAPI/Controllers/Models/RegisterRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LicentaAPI.Controllers.Models
+{
+    /// <summary>
+    /// All information that is needed to register a user.
+    /// </summary>
+    public class RegisterRequest
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+
+        [Required]
+        public string Sex { get; set; }
+    }
+}
