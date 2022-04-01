47cacec553c7d2035ad3c3eedcce737d82902f75
craciun-adriana <81712027+craciun-adriana@users.noreply.github.com>
2022-04-01 16:43:16 +0300

    Added film create to controller.

---
+++ LicentaAPI/Controllers/FilmController.cs
@@ -0,0 +1,55 @@
+﻿using LicentaAPI.AppServices.Films;
+using LicentaAPI.AppServices.Films.Models;
+using LicentaAPI.Controllers.Models;
+using LicentaAPI.Infrastructure.Mapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace LicentaAPI.Controllers
+{
+    [ApiController]
+    [Route("licenta/film")]
+    public class FilmController : ControllerBase
+    {
+        public readonly IFilmService _filmService;
+        public readonly IMappingCoordinator _mapper;
+
+        public FilmController(IFilmService filmService, IMappingCoordinator mapper)
+        {
+            _filmService = filmService;
+            _mapper = mapper;
+        }
+
+        [Authorize]
+        [HttpPost("create-film")]
+        [SwaggerResponse(201, "Film was created.")]
+        [SwaggerResponse(404, "Film can't be created.")]
+        public IActionResult CreateFilm(FilmCreateRequest request)
+        {
+            var filmCreate = _mapper.Map<FilmCreateRequest, FilmCreate>(request);
+
+            var film = _filmService.CreateFilm(filmCreate);
+            if (film == null)
+            {
+                return CreateResponse(500, new { Error = "DB Error." });
+            }
+
+            return CreatedAtRoute("", new { film.ID }, film);
+        }
+
+        private IActionResult CreateResponse<T>(int statusCode, T content)
+        {
+            if (statusCode >= 400)
+            {
+                var errorResponse = BadRequest(content);
+                errorResponse.StatusCode = statusCode;
+                return errorResponse;
+            }
+
+            var response = Ok(content);
+            response.StatusCode = statusCode;
+            return response;
+        }
+    }
+}
---
+++ LicentaAPI/Controllers/Models/FilmCreateRequest.cs
@@ -0,0 +1,40 @@
+﻿using LicentaAPI.Persistence.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LicentaAPI.Controllers.Models
+{
+    public class FilmCreateRequest
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime RelaseDate { get; set; }
+
+        [ForeignKey(nameof(Film))]
+        public string PrequelID { get; set; }
+
+        [ForeignKey(nameof(Film))]
+        public string SequelID { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        public Rating Rating { get; set; }
+
+        [Required]
+        public int Length { get; set; }
+    }
+}
