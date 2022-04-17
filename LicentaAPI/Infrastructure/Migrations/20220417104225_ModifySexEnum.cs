6b509eb0713943f5f5f8077200d90b0e98f727f6
craciun-adriana <81712027+craciun-adriana@users.noreply.github.com>
2022-04-17 13:57:38 +0300

    Modify Sex to be enum type.

---
+++ LicentaAPI/Infrastructure/Migrations/20220417104225_ModifySexEnum.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LicentaAPI.Infrastructure.Migrations
+{
+    public partial class ModifySexEnum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Sex",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Sex",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+    }
+}
