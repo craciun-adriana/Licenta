6c865b33f0c7569871be34d33579686b420dc037
craciun-adriana <81712027+craciun-adriana@users.noreply.github.com>
2022-05-04 23:49:00 +0300

    Added group create to Controller.

---
+++ LicentaAPI/Controllers/GroupController.cs
@@ -0,0 +1,43 @@
+﻿using LicentaAPI.AppServices.Groups;
+using LicentaAPI.AppServices.Groups.Model;
+using LicentaAPI.Controllers.Models;
+using LicentaAPI.Infrastructure.Mapper;
+using LicentaAPI.Persistence.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace LicentaAPI.Controllers
+{
+    [ApiController]
+    [Route("licenta/group")]
+    public class GroupController : BaseController
+    {
+        private readonly IGroupService _groupService;
+        private readonly IMappingCoordinator _mapper;
+
+        public GroupController(IGroupService groupService, UserManager<AppUser> userManager, IMappingCoordinator mapper) : base(userManager)
+        {
+            _groupService = groupService;
+            _mapper = mapper;
+        }
+
+        [Authorize]
+        [HttpPost("create")]
+        [SwaggerResponse(201, "Group was created.")]
+        [SwaggerResponse(404, "Group can't be created.")]
+        public IActionResult CreateGroup(GroupCreateRequest request)
+        {
+            var groupCreate = _mapper.Map<GroupCreateRequest, GroupCreate>(request);
+            var group = _groupService.CreateGroup(groupCreate);
+
+            if (group == null)
+            {
+                return CreateResponse(500, new { Error = "DB Error." });
+            }
+
+            return CreatedAtRoute("", new { group.ID }, group);
+        }
+    }
+}
---
+++ LicentaAPI/Controllers/Models/GroupCreateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LicentaAPI.Controllers.Models
+{
+    public class GroupCreateRequest
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Picture { get; set; }
+
+        public string Description { get; set; }
+    }
+}
