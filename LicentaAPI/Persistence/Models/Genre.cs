45283a0b747162ec09c58707f0c8f1703554f61b
craciun-adriana <81712027+craciun-adriana@users.noreply.github.com>
2022-05-05 23:11:01 +0300

    Added more genres.

--- LicentaAPI/Persistence/Models/Genre.cs
+++ LicentaAPI/Persistence/Models/Genre.cs
@@ -2,8 +2,16 @@
 {
     public enum Genre
     {
-        Action,
-        Comedy,
-        Drama,
+        Action = 0,
+        Comedy = 1,
+        Crime = 2,
+        Drama = 3,
+        Fantasy = 4,
+        Horror = 5,
+        Muzical = 6,
+        Romance = 7,
+        ScienceFiction = 8,
+        Thriller = 9,
+        Western = 10,
     }
 }
